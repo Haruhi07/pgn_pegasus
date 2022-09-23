ae1283f7d2156936410ad7721819e81ef0e1c1c1
yyx <yuxuan.ye@outlook.com>
2022-09-24 00:25:10 +0100

    evaluate random pgn_pegasus

--- eval.py
+++ eval.py
@@ -1,9 +1,17 @@
-import tqdm
+from tqdm import tqdm
 import json
 import torch
+import argparse
 import evaluate
+from utils import set_random_seed
 from pathlib import Path
+from Model import PointerPegasus
+from torch.utils.data import DataLoader, SequentialSampler
+from datasets import load_from_disk
+from transformers import PegasusTokenizer
 
+seed = 42
+set_random_seed(42)
 
 results_dir = Path("./results")
 
@@ -35,3 +43,25 @@ def eval(model, tokenizer, eval_loader, device):
     with open(results_dir/"result.json", "w") as fp:
         json.dump(results, fp)
     return results
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--model", default=None)
+    parser.add_argument("--dataset", required=True)
+    args = parser.parse_args()
+
+    device = 'cuda' if torch.cuda.is_available() else 'cpu'
+    checkpoint = 'google/pegasus-cnn_dailymail'
+    tokenizer = PegasusTokenizer.from_pretrained(checkpoint)
+    model = PointerPegasus(checkpoint, tokenizer, device).to(device)
+    if args.model is not None:
+        model.load_state_dict(args.model).to(device)
+
+    tokenized_test = load_from_disk(args.dataset)
+    test_loader = DataLoader(tokenized_test,
+                             sampler=SequentialSampler(tokenized_test),
+                             num_workers=2,
+                             batch_size=1)
+
+    eval(model, tokenizer, test_loader, device)
---
+++ run_eval.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+#SBATCH --nodes=1
+#SBATCH --gres=gpu:1
+#SBATCH --partition gpu
+#SBATCH --job-name=pgn_pegasus
+#SBATCH --time=1:0:0
+#SBATCH --mem=8192M
+
+cd "${SLURM_SUBMIT_DIR}"
+
+python3 -u ./eval.py --dataset ./dataset_cache
