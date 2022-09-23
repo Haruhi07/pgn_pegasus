d08db3bf68593a3be46c2f63f28322a6a045b994
Ubuntu <yuxuan.ye@outlook.com>
2022-09-23 13:57:36 +0100

    fix slurm script

--- pgn_pegasus.sh
+++ pgn_pegasus.sh
@@ -2,8 +2,6 @@
 #SBATCH --nodes=1
 #SBATCH --gres=gpu:1
 #SBATCH --partition gpu
-#SBATCH --ntask-per-node=1
-#SBATCH --cpus-per-task=1
 #SBATCH --job-name=pgn_pegasus
 #SBATCH --time=0:20:0
 #SBATCH --mem=8192M
