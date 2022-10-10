51588f1234fadf571f82e13603bc0f2a3d6425ae
Ubuntu <yuxuan.ye@outlook.com>
2022-10-10 15:39:43 +0100

    re-define the dataset class

--- generate_dataset.py
+++ generate_dataset.py
@@ -1,29 +1,37 @@
 import sys
+import pickle
+from tqdm import tqdm
 from pathlib import Path
 from datasets import load_dataset
 from transformers import PegasusTokenizer
+from torch.utils.data.dataset import Dataset
+
+class Pegasus_Dataset(Dataset):
+    def __init__(self, dataset, tokenizer, max_length=128, max_source_length=512):
+        self.dataset = dataset
+        for idx, sample in enumerate(tqdm(dataset)):
+            input_dict = tokenizer(sample["article"],
+                                   truncation=True,
+                                   max_length=max_source_length,
+                                   return_tensors='pt')
+            decoder_input_dict = tokenizer(tokenizer.pad_token+ sample["highlights"],
+                                           truncation=True,
+                                           max_length=max_length,
+                                           return_tensors='pt')
+            self.dataset[idx]["input_dict"] = input_dict
+            self.dataset[idx]["decoder_input_dict"] = decoder_input_dict
+
+    def __getitem__(self, item):
+        return self.dataset[item]
+
+    def __len__(self):
+        return len(self.dataset)
+
 
 cache_dir = Path("./cache")
 dataset_dir = Path("./dataset_cache")
-max_length=128
-max_source_length=1024
 tokenizer = PegasusTokenizer.from_pretrained("google/pegasus-cnn_dailymail")
 
-
-def tokenize_dataset(sample):
-    input_dict = tokenizer(sample["article"],
-                           truncation=True,
-                           max_length=max_source_length,
-                           return_tensors='pt')
-    decoder_input_dict = tokenizer(tokenizer.pad_token + sample["highlights"],
-                                   truncation=True,
-                                   max_length=max_length,
-                                   return_tensors='pt')
-    sample["input_ids"] = input_dict["input_ids"]
-    sample["attention_mask"] = input_dict["attention_mask"]
-    sample["decoder_input_ids"] = decoder_input_dict["input_ids"]
-    sample["decoder_attention_mask"] = decoder_input_dict["attention_mask"]
-    return sample
 
 sys.stderr.write("before loading")
 training = load_dataset("cnn_dailymail", "3.0.0", split="train", cache_dir=cache_dir)
@@ -31,10 +39,13 @@ validation = load_dataset("cnn_dailymail", "3.0.0", split="validation", cache_di
 test = load_dataset("cnn_dailymail", "3.0.0", split="test", cache_dir=cache_dir)
 
 sys.stderr.write("after loading")
-tokenized_training = training.map(tokenize_dataset)
-tokenized_validation = validation.map(tokenize_dataset)
-tokenized_test = test.map(tokenize_dataset)
+tokenized_training = Pegasus_Dataset(training, tokenizer)
+tokenized_validation = Pegasus_Dataset(validation, tokenizer)
+tokenized_test = Pegasus_Dataset(test, tokenizer)
 
-tokenized_training.save_to_disk(dataset_dir)
-tokenized_validation.save_to_disk(dataset_dir)
-tokenized_test.save_to_disk(dataset_dir)
+with open(dataset_dir/"tokenized_training.json", "wb") as fp:
+    pickle.dump(tokenized_training, fp)
+with open(dataset_dir/"tokenized_validation.json", "wb") as fp:
+    pickle.dump(tokenized_validation, fp)
+with open(dataset_dir/"tokenized_test.json", "wb") as fp:
+    pickle.dump(tokenized_test, fp)
