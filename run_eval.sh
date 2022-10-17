75bb9864842a20f733626bd930449e500bd2effb
yyx <yuxuan.ye@outlook.com>
2022-10-17 12:54:27 +0100

    eval model after training 1 epoch

--- run_eval.sh
+++ run_eval.sh
@@ -9,4 +9,4 @@
 cd "${SLURM_SUBMIT_DIR}"
 
 source venv/bin/activate
-python3 -u ./eval.py --dataset ./dataset_cache --checkpoint ./checkpoint/epoch_0.pt --epoch 0
+python3 -u ./eval.py --dataset ./dataset_cache --checkpoint ./checkpoints/epoch_0.pt --epoch 0
